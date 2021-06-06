9bf93d1154bfe29e804237e32648e4c78f432602
Adriano Raiano <adriano@raiano.ch>
2021-06-06 18:25:09 +0200

    vue-i18n example

---
+++ example/vue/src/components/TranslationShowCase.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1>{{ $t("welcome") }}</h1>
+    <p>
+      <i18n-t keypath="descr" tag="label" for="doc">
+        <a href="https://cli.vuejs.org" target="_blank">{{ $t('doc') }}</a>
+      </i18n-t>
+    </p>
+    <div>
+      <div>
+        <span v-t="{path:'end'}" /> <i v-t="'end'" />
+      </div>
+    </div>
+    <hr />
+    <div>
+      <div>
+        <a v-if="$i18n.locale !== 'de'" v-on:click="changeLanguage('de')">
+          DE
+        </a>
+        <strong v-if="$i18n.locale === 'de'">
+          DE
+        </strong>
+        &nbsp;|&nbsp;
+        <a v-if="$i18n.locale !== 'en'" v-on:click="changeLanguage('en')">
+          EN
+        </a>
+        <strong v-if="$i18n.locale === 'en'">
+          EN
+        </strong>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TranslationShowCase',
+  methods: {
+    changeLanguage(lang) {
+      this.$i18n.locale = lang;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ example/vue/src/i18n.js
@@ -0,0 +1,24 @@
+import { createI18n } from 'vue-i18n'
+import locizer from 'locizer'
+
+locizer.init({
+  projectId: '1fb2c780-f038-4ffe-b988-5d9fe2e3ba2d'
+});
+
+export const i18n = createI18n({
+  locale: locizer.lng, // set locale
+  fallbackLocale: 'en' // set fallback locale
+  // If you need to specify other options, you can set other options
+  // ...
+});
+
+
+export const loadMessagesPromise = new Promise((resolve, reject) => {
+  locizer.loadAll('messages', (err, messages) => {
+    if (err) return reject(err);
+    Object.keys(messages).forEach((l) => {
+      i18n.global.setLocaleMessage(l, messages[l])
+    });
+    resolve(messages);
+  });
+})
